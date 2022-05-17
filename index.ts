b1e37e50952ce043fee85156a4e464f34ca4fecd
fxrcha <truetype24@gmail.com>
2022-05-17 09:25:34 +0900

    feat: setting structures

--- index.ts
+++
@@ -1,3 +0,0 @@
-import chalk from 'chalk';
-
-console.log(chalk.bold('H1PST3RRR'))
---
+++ src/instance/index.ts
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+class HipsterInstance {
+    constructor() {
+
+    }
+}
+
+export default HipsterInstance;
